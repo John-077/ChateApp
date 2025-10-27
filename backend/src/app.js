d5f747590913d02b611845edd116685c4e76dc80
yohanisHs <johnazusa5@gmail.com>
2025-10-27 17:32:57 +0300

    welcome email implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,12 @@ import path from "path";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { ENV } from "./lib/env.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = ENV.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use(express.json());// middleware to parse JSON request bodies
@@ -18,12 +19,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "..","frontend", "dist", "index.html"));
-  // });
+
 
   app.use((_, res) => {
    
@@ -47,4 +46,3 @@ const startServer = async () => {
 };
 
 startServer();
-
