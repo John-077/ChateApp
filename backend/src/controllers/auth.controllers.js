1c1d66d822734be7c25766a7e64409db2287b622
yohanisHs <johnazusa5@gmail.com>
2025-10-28 18:17:54 +0300

    add

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -73,3 +73,38 @@ export const signup = async (req, res) => {
     
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credential" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credential" });
+    }
+    generateToken(user._id, res);
+    return res
+      .status(200)
+      .json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic,
+      });
+  } catch (error) {
+    console.error("Error in login controller:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+
+};
+
+export const logout = (_, res) => {
+ res.cookie("jwt","",{maxAge:0})
+  res.status(200).json({message:"Logged out successfully"})
+}
