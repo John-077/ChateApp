3e358fec88de41c2621b0110449775299241f47a
yohanisHs <johnazusa5@gmail.com>
2025-10-28 18:38:34 +0300

    login & logout functionality added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controllers.js";
+import { signup,login,logout } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
 
 
 
