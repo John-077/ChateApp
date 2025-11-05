4828f8e89e8d28208246034fc84643580df9950d
yohanisHs <johnazusa5@gmail.com>
2025-11-05 16:06:38 +0300

    message routes optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -40,6 +40,17 @@ export const sendMessages = async (req, res) => {
     const { id:receiverId } = req.params;
     const senderId = req.user._id;
 
+    if (!text && !image) {
+      return res.status(400).json({ error: " Text or image is required" });
+    }
+    if(senderId.equals(receiverId)){
+      return res.status(400).json({ error: "Cannot send message to yourself" });
+    }
+    const receiverExists = await User.exists({_id:receiverId});
+    if(!receiverExists){
+      return res.status(404).json({ error: "Receiver not found" });
+    }
+
     let imageUrl;
     if (image) {
       // upload base64 image to cloudinary
