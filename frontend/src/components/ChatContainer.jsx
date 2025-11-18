883f14f33592a090606c4c4409ff090c69740b29
yohanisHs <johnazusa5@gmail.com>
2025-11-18 11:25:40 +0300

    chat container update

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -17,15 +17,13 @@ function ChatContainer() {
   return (
     <>
       <ChatHeader />
-      <div className="flex px-6 overflow-y-auto py-8">
+      <div className="flex-1 px-6 overflow-y-auto py-8">
         {messages.length > 0 && !isMessagesLoading ? (
-          <div className="max-w-3xl max-auto space-y-6">
+          <div className="max-w-3xl mx-auto space-y-6">
             {messages.map((msg) => (
               <div
                 key={msg._id}
-                className={`chat ${
-                  msg.senderId === authUser._id ? "chat-end" : "chat-start"
-                }`}
+                className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
               >
                 <div
                   className={`chat-bubble relative ${
@@ -35,11 +33,7 @@ function ChatContainer() {
                   }`}
                 >
                   {msg.image && (
-                    <img
-                      src={msg.image}
-                      alt="Shared"
-                      className="rounded-lg h-48 object-cover"
-                    />
+                    <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
                   )}
                   {msg.text && <p className="mt-2">{msg.text}</p>}
                   <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
@@ -51,6 +45,8 @@ function ChatContainer() {
                 </div>
               </div>
             ))}
+            {/* 👇 scroll target */}
+            {/* <div ref={messageEndRef} /> */}
           </div>
         ) : isMessagesLoading ? (
           <MessagesLoadingSkeleton />
@@ -58,8 +54,10 @@ function ChatContainer() {
           <NoChatHistoryPlaceholder name={selectedUser.fullName} />
         )}
       </div>
+
       <MessageInput />
     </>
+
   );
 }
 
